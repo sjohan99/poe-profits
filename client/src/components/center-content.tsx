80ad9dcacce8800162d41d1deec2e6ef687786d2
Johan Selin <johanselin99@gmail.com>
2024-10-21 00:02:13 +0200

    Fix overflow issue from flex

--- client/src/components/center-content.tsx
+++ client/src/components/center-content.tsx
@@ -9,16 +9,14 @@ export function CenterContent(props: CenterContentProps) {
   const extra_styling = props.className ?? "";
   const childrenContainerClassName = props.childrenContainerClassName ?? "";
   return (
-    <div className={`flex justify-center ${extra_styling}`.trim()}>
-      <div className="flex-none -lg:min-w-0"></div>
-      <div
-        className={`max-w-screen-2xl grow rounded-sm ${props.background ?? ""}`.trim()}
-      >
-        <div className={`px-3 py-3 ${childrenContainerClassName}`}>
-          {props.children}
+    <div className="flex justify-center">
+      <div className={`w-full max-w-screen-2xl ${extra_styling}`.trim()}>
+        <div className={`rounded-sm ${props.background ?? ""}`.trim()}>
+          <div className={`px-3 py-3  ${childrenContainerClassName}`}>
+            {props.children}
+          </div>
         </div>
       </div>
-      <div className="flex-none -lg:min-w-0"></div>
     </div>
   );
 }
