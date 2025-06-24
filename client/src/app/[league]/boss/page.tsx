5b31595a0a9590e210f7666a8aadcd75165280b6
Johan Selin <johanselin99@gmail.com>
2025-06-24 22:06:37 +0200

    Client: Remove drop rate warnings

--- client/src/app/[league]/boss/page.tsx
+++ client/src/app/[league]/boss/page.tsx
@@ -14,13 +14,6 @@ export default async function Page({ params }: { params: { league: string } }) {
 
   return (
     <>
-      {params.league.toLowerCase().includes("mercenaries") && (
-        <h2 className="text-orange-500">
-          <span className="text-orange-300">Searing Exarch</span> and{" "}
-          <span className="text-orange-300">Maven</span> drop rates have changed
-          in 3.26. Their drop rates are being estimated and may be inaccurate.
-        </h2>
-      )}
       <LeagueSelector league={params.league} route="boss"></LeagueSelector>
       <SummaryDataTable bosses={bosses} league={params.league} />
     </>
