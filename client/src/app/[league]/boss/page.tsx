d8c36915e2dd0e11525787d437d8c5ac5f5baa0b
Johan Selin <johanselin99@gmail.com>
2025-11-02 20:46:36 +0100

    Client: Add new league warning

--- client/src/app/[league]/boss/page.tsx
+++ client/src/app/[league]/boss/page.tsx
@@ -19,7 +19,24 @@ export default async function Page({ params }: { params: { league: string } }) {
   return (
     <>
       <LeagueSelector league={params.league} route="boss"></LeagueSelector>
+      <DroprateWarning league={params.league} />
       <SummaryDataTable bosses={bosses} league={params.league} />
     </>
+  );
+}
+
+function DroprateWarning({ league }: { league: string }) {
+  if (!league.toLowerCase().includes("keepers")) return null;
+  return (
+    <div>
+      <h2 className="text-orange-500">
+        New league warning: Drop rates may have changed and item prices are
+        volatile.
+      </h2>
+      <h2 className="text-orange-300">
+        Known issues: Incarnation bosses have not been updated for the new
+        Keepers league yet.
+      </h2>
+    </div>
   );
 }
